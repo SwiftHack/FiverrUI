ff13989d2f4f7c311bbc0caf232e544514146c14
Samuel Uyigue <Samuel.uyigue@gmail.com>
2022-08-19 14:53:47 -0500

    Initial Commit

---
+++ FiverrUI/FiverrUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiverrUIApp.swift
+//  FiverrUI
+//
+//  Created by Osaretin Uyigue on 8/19/22.
+//
+
+import SwiftUI
+
+@main
+struct FiverrUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
