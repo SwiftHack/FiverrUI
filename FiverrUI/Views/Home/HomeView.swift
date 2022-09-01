1cd77e75c492df72617ea57de5ebe0061fe78b99
Samuel Uyigue <Samuel.uyigue@gmail.com>
2022-08-31 22:35:31 -0500

    Building HomeView UI

--- FiverrUI/Views/Home/HomeView.swift
+++ FiverrUI/Views/Home/HomeView.swift
@@ -8,13 +8,94 @@
 import SwiftUI
 
 struct HomeView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                HStack {
+                    Text("Popular services")
+                        .font(.headline)
+                    
+                    Spacer()
+                    
+                    Button(action: {}) {
+                        Text("See All")
+                            .font(.subheadline)
+                        
+                    }
+                }.padding(12)
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(0..<10) {_ in
+                            PoularServicesView()
+                        }
+                    }.padding(12)
+                        .padding(.top, -12)
+                }
+                
+                
+                Image("fiverImage2")
+                    .resizable()
+                    .scaledToFit()
+                    .overlay(
+                        VStack {
+                            Spacer()
+                            Text("Explore beautiful work, picked for you.")
+                                .foregroundColor(.white)
+                                .font(.title2)
+                                .padding()
+                        }
+                    )
+                    .clipped()
+                    .cornerRadius(8)
+                    .padding()
+                    
+                
+            }
+            
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Image("fiverlogo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 25)
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Image(systemName: "hands.sparkles.fill")
+                        .resizable()
+                }
+            }
+        }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        MainTabView()
+    }
+}
+
+struct PoularServicesView: View {
+    private let cardWidth: CGFloat = 125
+    
+    var body: some View {
+        VStack {
+            Image("mockImage")
+                .resizable()
+                .scaledToFill()
+                .frame(height: 100)
+                .clipped()
+            Spacer()
+            Text("Logo Design")
+                .font(.system(size: 14, weight: .semibold))
+            Spacer()
+        }
+        .background(Color.white)
+        .frame(width: cardWidth, height: 180)
+        .cornerRadius(8)
+        .shadow(color: Color(.systemGray3), radius: 5, x: 0, y: 2)
     }
 }
--- FiverrUI/Views/main/MainTabView.swift
+++ FiverrUI/Views/main/MainTabView.swift
@@ -23,7 +23,7 @@ struct MainTabView: View {
     var body: some View {
         TabView(selection: $selectedTab) {
             
-            Text(Tab.home.description)
+            HomeView()
                 .tabItem {
                         
                     Image(systemName: selectedTab == .home ? "house.fill": "house")
